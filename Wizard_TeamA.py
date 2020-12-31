cf0342ece215fe6e95926014d2991072757d3e07
Abicharan <abicharan.b@gmail.com>
2020-12-31 14:03:00 +0800

    Fixed desire equations and added comments

--- Wizard_TeamA.py
+++ Wizard_TeamA.py
@@ -195,22 +195,31 @@ class WizardStateHealing_TeamA(State):
         pass
 
 
-
-'''
+"""
     Experimental features
     Uses goal based design 
     Evaluates which goal is the most impt
-'''
-class character_feature(object):
-    """
+
     Calculates ratings for the different level up attributes
     Note: This is an experimentation, equation is subject to change
 
-    """
+"""
+
+class character_feature(object):
+
+    '''
+        Equation = current_hp over max_hp
+    '''
     def get_rating_health(character:Character)->int:
         health = (character.current_hp / character.max_hp) * 1
         return min(1, health)
 
+
+    '''
+        Equation = (damage/cooldown)/damage
+        percentage of ranged_damage it output per second
+       
+    '''
     def get_weapon_damage_per_second(character:Character)->int:
         
         rating = (character.ranged_damage/character.ranged_cooldown)/character.ranged_damage
@@ -218,22 +227,45 @@ class character_feature(object):
 
 
 def health_level_up_evaluate(character:Character) -> int:
-    '''How powerful is the bot feeling?'''
-    tweaker = 0.1 #how important is this level up attribute to the hero
+    '''
+        How healthy is the bot?
+        Equation:
 
-    desire = tweaker * ((1-character_feature.get_rating_health(character))/character.healing_cooldown)
+        tweaker * ( 1 - rating of the health of the character) multiplied by its healing cooldown 
+
+        Thus if the bot is low in health and it cooldown is 4 seconds,
+        it will desire to upgrade its healing more 
+        than if the cooldown was 0.4 seconds
+    '''
+    tweaker = 0.2 #how important is this level up attribute to the hero
+
+    desire = tweaker * ((1-character_feature.get_rating_health(character)) * character.healing_cooldown)
     return [min(1,desire), 'hp']
 
 
 def damage_level_up_evaluate(character:Character) -> int:
-    '''How far is it from the base?'''
-    tweaker = 0.25
+    '''
+        How far is it from the base?
 
+        Equation: 
+        (tweaker * ( 1 - rating of the damage of the character) * health) divided by the
+        distance away from an enemy base)
+
+        the closer to the enemy base the more it would want to upgrade
+        its damage output
+    '''
+    tweaker = 0.25
     #nearer to the base to more I want to upgrade my damage output
     enemy_base = g.get_enemy_base(character)
     dist_base_and_enemy_base = g.distance_between(g.get_friendly_base(character).position, enemy_base.position)
     dist = g.distance_between(character.position, enemy_base.position)
-    desire = (tweaker  *(1- character_feature.get_weapon_damage_per_second(character)))/(dist/dist_base_and_enemy_base)
+    dist_normalized = dist/dist_base_and_enemy_base
+
+    rating_health = character_feature.get_rating_health(character)
+    rating_damage = character_feature.get_weapon_damage_per_second(character)
+
+    desire = (tweaker * rating_health * (1-rating_damage))/dist_normalized
+    print(desire)
     return [min(1,desire), 'ranged damage']
 
 
@@ -242,4 +274,5 @@ def get_best_score(
     hero: Character,
     *predicates)->str:
     ''' ooo functional programming ehuehue'''
+
     return [max(pred(hero) for pred in predicates)][0][1]
