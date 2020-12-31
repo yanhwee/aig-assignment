0ef2e459f518d8650d0be02a99875ddc6be18b76
Abicharan <abicharan.b@gmail.com>
2020-12-31 15:52:53 +0800

    Fixed level up bugz

--- Wizard_TeamA.py
+++ Wizard_TeamA.py
@@ -48,8 +48,9 @@ class Wizard_TeamA(Character):
         
         level_up_stats = ["hp", "speed", "ranged damage", "ranged cooldown", "projectile range"]
         if self.can_level_up():
-            choice = randint(0, len(level_up_stats) - 1)
-            self.level_up(level_up_stats.index(best))      
+            self.level_up(best)
+
+               
 
 
 
