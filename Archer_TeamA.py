f48e137654f878ee87042ab8897c31bd99e016bc
Yan Hwee Yee <s10186981@connect.np.edu.sg>
2021-01-05 13:07:48 +0800

    updated archer v2

--- Archer_TeamA.py
+++ Archer_TeamA.py
@@ -5,9 +5,14 @@ from Character import *
 from State import *
 import g
 
-DEFAULT_PATH = 3
-PROJECTILE_RETREAT_RADIUS = 150
-LOW_HP = 190
+DEFAULT_PATH = 0
+SKIRMISHING_PROJECTILE_RETREAT_RADIUS = 150
+SKIRMISHING_ENEMY_RETREAT_RADIUS = 150
+HEALING_NO_ENEMY_RADIUS = 160
+HEALING_PROJECTILE_RETREAT_RADIUS = 150
+HEALING_ENEMY_RETREAT_RADIUS = 220
+LOW_HP = 199
+HIGH_HP = 200
 
 class Archer_TeamA(Character):
     def __init__(self, world, image, projectile_image, base, position):
@@ -20,17 +25,13 @@ class Archer_TeamA(Character):
         # G Init
         g.init_hero(self)
         # State Machine
-        seeking_state = ArcherStateSeeking_TeamA(self)
         skirmishing_state = ArcherStateSkirmishing_TeamA(self)
-        retreating_state = ArcherStateRetreating_TeamA(self)
         healing_state = ArcherStateHealing_TeamA(self)
         ko_state = ArcherStateKO_TeamA(self)
-        self.brain.add_state(seeking_state)
         self.brain.add_state(skirmishing_state)
-        self.brain.add_state(retreating_state)
         self.brain.add_state(healing_state)
         self.brain.add_state(ko_state)
-        self.brain.set_state("seeking")
+        self.brain.set_state('skirmishing')
 
     def render(self, surface):
         Character.render(self, surface)
@@ -40,9 +41,9 @@ class Archer_TeamA(Character):
         if self.can_level_up():
             self.level_up('ranged cooldown')
 
-class ArcherStateSeeking_TeamA(State):
+class ArcherStateSkirmishing_TeamA(State):
     def __init__(self, archer):
-        State.__init__(self, 'seeking')
+        State.__init__(self, 'skirmishing')
         self.archer = archer
 
     def entry_actions(self):
@@ -56,103 +57,41 @@ class ArcherStateSeeking_TeamA(State):
             g.switch_to_path(self.archer, path_index)
 
     def do_actions(self):
-        # Go towards enemy base
+        # Get Targets
         enemy_base = g.get_enemy_base(self.archer)
-        if enemy_base:
+        enemy = g.get_nearest_enemy_that_is(self.archer,
+            lambda entity: g.within_range_of_target(self.archer, entity),
+            lambda entity: g.in_sight_with_preaimed_target(self.archer, entity))
+        projectile = g.get_nearest_non_friendly_projectile_that_is(self.archer,
+            lambda entity: g.within_range_of_target(self.archer, entity, SKIRMISHING_PROJECTILE_RETREAT_RADIUS),
+            lambda entity: g.in_sight_with_target(self.archer, entity))
+        # Attack
+        if g.within_range_of_target(self.archer, enemy_base):
+            self.archer.ranged_attack(enemy_base.position)
+        elif enemy:
+            preaim_position = g.preaim_entity(self.archer, enemy)
+            self.archer.ranged_attack(preaim_position)
+        # Move
+        if projectile:
+            path_pos = g.position_away_from_target_using_path(self.archer, projectile)
+            g.set_move_target(self.archer, path_pos)
+        elif enemy is None:
             path_pos = g.position_towards_target_using_path(self.archer, enemy_base)
             g.set_move_target(self.archer, path_pos)
-        # Updates Velocity
-        g.update_velocity(self.archer)
-
-    def check_conditions(self):
-        # Checks for non-friendly (enemy and neutral) projectiles
-        enemy_projectile = g.get_nearest_non_friendly_projectile_that_is(self.archer,
-            lambda entity: g.within_range_of_target(self.archer, entity, radius=PROJECTILE_RETREAT_RADIUS),
-            lambda entity: g.in_sight_with_target(self.archer, entity))
-        if enemy_projectile:
-            return 'retreating'
-        # Checks for low hp
-        if self.archer.current_hp <= LOW_HP:
-            return 'healing'
-        # Checks for enemy
-        enemy = g.get_nearest_enemy_that_is(self.archer,
-            lambda entity: g.within_range_of_target(self.archer, entity),
-            lambda entity: g.in_sight_with_preaimed_target(self.archer, entity))
-        if enemy:
-            return 'skirmishing'
-        return None
-
-class ArcherStateSkirmishing_TeamA(State):
-    def __init__(self, archer):
-        State.__init__(self, 'skirmishing')
-        self.archer = archer
-
-    def entry_actions(self):
-        pass
-
-    def do_actions(self):
-        # Checks for enemy
-        enemy = g.get_nearest_enemy_that_is(self.archer,
-            lambda entity: g.within_range_of_target(self.archer, entity),
-            lambda entity: g.in_sight_with_preaimed_target(self.archer, entity))
-        if enemy:
-            # Preaim Enemy Position
-            preaim_position = g.preaim_entity(self.archer, enemy)
-            # Shoot projectile
-            self.archer.ranged_attack(preaim_position)
-            # Move Away from target
+        elif g.within_range_of_target(self.archer, enemy, SKIRMISHING_ENEMY_RETREAT_RADIUS):
             path_pos = g.position_away_from_target_using_path(self.archer, enemy)
             g.set_move_target(self.archer, path_pos)
-        # Update Velocity
+        else:
+            g.set_move_target(self.archer, None)
         g.update_velocity(self.archer)
 
     def check_conditions(self):
-        # Checks for non-friendly (enemy and neutral) projectiles
-        enemy_projectile = g.get_nearest_non_friendly_projectile_that_is(self.archer,
-            lambda entity: g.within_range_of_target(self.archer, entity, radius=PROJECTILE_RETREAT_RADIUS),
-            lambda entity: g.in_sight_with_target(self.archer, entity))
-        if enemy_projectile:
-            return 'retreating'
-        # Checks for low hp
         if self.archer.current_hp <= LOW_HP:
-            return 'healing'
-        # Checks for enemy
-        enemy = g.get_nearest_enemy_that_is(self.archer,
-            lambda entity: g.within_range_of_target(self.archer, entity),
-            lambda entity: g.in_sight_with_preaimed_target(self.archer, entity))
-        if enemy is None:
-            return 'seeking'
-        return None
-
-class ArcherStateRetreating_TeamA(State):
-    def __init__(self, archer):
-        State.__init__(self, 'retreating')
-        self.archer = archer
-
-    def entry_actions(self):
-        pass
-
-    def do_actions(self):
-        # Checks for non-friendly projectiles
-        enemy_projectile = g.get_nearest_non_friendly_projectile_that_is(self.archer,
-            lambda entity: g.within_range_of_target(self.archer, entity, radius=PROJECTILE_RETREAT_RADIUS),
-            lambda entity: g.in_sight_with_target(self.archer, entity))
-        if enemy_projectile:
-            path_pos = g.position_away_from_target_using_path(self.archer, enemy_projectile)
-            g.set_move_target(self.archer, path_pos)
-        # Updates Velocity
-        g.update_velocity(self.archer)
-
-    def check_conditions(self):
-        # Checks for low hp
-        if self.archer.current_hp <= LOW_HP:
-            return 'healing'
-        # Checks for non-friendly projectiles
-        enemy_projectile = g.get_nearest_non_friendly_projectile_that_is(self.archer,
-            lambda entity: g.within_range_of_target(self.archer, entity, radius=PROJECTILE_RETREAT_RADIUS),
-            lambda entity: g.in_sight_with_target(self.archer, entity))
-        if enemy_projectile is None:
-            return 'seeking'
+            enemy = g.get_nearest_enemy_that_is(self.archer,
+                lambda entity: g.within_range_of_target(self.archer, entity, HEALING_NO_ENEMY_RADIUS),
+                lambda entity: g.in_sight_with_target(self.archer, entity))
+            if enemy is None:
+                return 'healing'
         return None
 
 class ArcherStateHealing_TeamA(State):
@@ -165,22 +104,34 @@ class ArcherStateHealing_TeamA(State):
 
     def do_actions(self):
         self.archer.heal()
-    
-    def check_conditions(self):
-        # Checks for non-friendly projectiles
-        enemy_projectile = g.get_nearest_non_friendly_projectile_that_is(self.archer,
-            lambda entity: g.within_range_of_target(self.archer, entity, radius=PROJECTILE_RETREAT_RADIUS),
-            lambda entity: g.in_sight_with_target(self.archer, entity))
-        if enemy_projectile:
-            return 'retreating'
-        # Checks for enemy
+        # Get Targets
+        enemy_base = g.get_enemy_base(self.archer)
         enemy = g.get_nearest_enemy_that_is(self.archer,
             lambda entity: g.within_range_of_target(self.archer, entity),
             lambda entity: g.in_sight_with_preaimed_target(self.archer, entity))
+        projectile = g.get_nearest_non_friendly_projectile_that_is(self.archer,
+            lambda entity: g.within_range_of_target(self.archer, entity, HEALING_PROJECTILE_RETREAT_RADIUS),
+            lambda entity: g.in_sight_with_target(self.archer, entity))
+        # Move
+        if projectile:
+            path_pos = g.position_away_from_target_using_path(self.archer, projectile)
+            g.set_move_target(self.archer, path_pos)
+        elif enemy is None:
+            path_pos = g.position_towards_target_using_path(self.archer, enemy_base)
+            g.set_move_target(self.archer, path_pos)
+        elif g.within_range_of_target(self.archer, enemy, HEALING_ENEMY_RETREAT_RADIUS):
+            path_pos = g.position_away_from_target_using_path(self.archer, enemy)
+            g.set_move_target(self.archer, path_pos)
+        g.update_velocity(self.archer)
+    
+    def check_conditions(self):
+        if self.archer.current_hp >= HIGH_HP:
+            return 'skirmishing'
+        enemy = g.get_nearest_enemy_that_is(self.archer,
+            lambda entity: g.within_range_of_target(self.archer, entity, HEALING_NO_ENEMY_RADIUS),
+            lambda entity: g.in_sight_with_target(self.archer, entity))
         if enemy:
             return 'skirmishing'
-        else:
-            return 'seeking'
         return None
 
 class ArcherStateKO_TeamA(State):
@@ -192,7 +143,7 @@ class ArcherStateKO_TeamA(State):
         return g.ko_do_actions(self.archer)
 
     def check_conditions(self):
-        return g.ko_check_conditions(self.archer, 'seeking')
+        return g.ko_check_conditions(self.archer, 'skirmishing')
 
     def entry_actions(self):
         return g.ko_entry_actions(self.archer)
